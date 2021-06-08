a55114d698e8bfe9e9227ef5a17255c736778771
sunnyswag <hhq126152@gmail.com>
2021-06-08 10:43:31 +0800

    finish_word_to_wordx

--- word_to_wordx/word_to_wordx.py
+++ word_to_wordx/word_to_wordx.py
@@ -1,13 +1,40 @@
-book = []
-with open("./word_to_wordx/book.txt", "r", encoding='utf-8') as f:
-    for line in f.readlines():
-        line = line.strip("\n")
-        if line != "" and line[0] != "无" and line[: 5] != '2021-'\
-            and line[: 4] != "笔记来自":
-            book.append(line)
+import re
 
-with open("./word_to_wordx/book_edited.txt", "w", encoding='utf-8') as f:
-    for i in range(len(book)):
-        f.write("{}.".format(i + 1) + book[i])
-        f.write('\n')
+def word_to_wordx(input_src: str) -> None:
+    """用来提取txt文本中有效信息的脚本
+    
+    Attributes:
+        input_src: 输入路径
+    """
+    book = []
+    book_output = []
+    output_src = re.findall(r"(.+?)book.txt", input_src)[0] + 'edited_book.txt'
+
+    with open(input_src, "r", encoding='utf-8') as f:
+        lines = f.readlines()
+        for i in range(len(lines)):
+            line = lines[i].strip("\n") # 去除换行符
+            if line != "" and line[0] != "无" and line[: 5] != '2021-'\
+                and line[: 4] != "笔记来自": # 去除无效的行
+                if line[0] == '【': # 将以'【'开头的行取消换行
+                    book[-1] += line
+                else: # 去除每行开头和结尾的标点符号
+                    if line[0] in ['，', '。', ',', '.', '：']:
+                        line = line[1:]
+                    if line[-1] in ['，', '。', ',', '.', '：']:
+                        line = line[:-1]
+                    book.append(line)
+
+    for line in book:
+        book_output.append(re.sub(r"【.*】", "", line))
             
+    with open(output_src, "w", encoding='utf-8') as f:
+        for i in range(len(book_output)):
+            f.write("{}. ".format(i + 1) + book_output[i])
+            f.write('\n')
+    
+    print("转换完成!")
+    print("输出路径为: {}".format(output_src))
+
+if __name__ == "__main__":
+    word_to_wordx("./word_to_wordx/book.txt")
